11a226f89a6fe1005ba856ae3aadf65bca903826
Ramana Jhammisetty <ramana.personal.work@gmail.com>
2025-09-05 13:32:55 +0530

    fix: adding karma files

---
+++ karma.conf.js
@@ -0,0 +1,12 @@
+module.exports = function (config) {
+  config.set({
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage']
+      }
+    },
+    singleRun: true, // prevents watcher mode in CI
+  });
+};
