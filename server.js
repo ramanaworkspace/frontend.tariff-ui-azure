7f90ddde482f07fadd577685f5eb44f8d1a15546
Ramana Jhammisetty <ramana.personal.work@gmail.com>
2025-09-12 17:59:16 +0530

    fix: updates on deployment file

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path');
 const app = express();
 
 const port = process.env.PORT || 8080;
-app.use(express.static(path.join(__dirname, 'dist/testcsrdeploy/browser')));
+app.use(express.static(path.join(__dirname, 'dist/tariff-calculation-ui/browser')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/testcsrdeploy/browser/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/tariff-calculation-ui/browser/index.html'));
 });
 app.listen(port, () => console.log(`Running on port ${port}`));
